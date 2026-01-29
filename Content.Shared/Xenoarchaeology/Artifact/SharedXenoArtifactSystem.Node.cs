da88276dc2ce52d34f3525cd2f12a0ad8c849aa6
ivo <mink4@proton.me>
2026-01-30 00:24:25 +0200

    Revert "Fix artifact point extraction going into negatives (#36642)"
    This reverts commit 13f71f75999a416d01622389dc5128c8980ae6d9.

--- Content.Shared/Xenoarchaeology/Artifact/SharedXenoArtifactSystem.Node.cs
+++ Content.Shared/Xenoarchaeology/Artifact/SharedXenoArtifactSystem.Node.cs
@@ -169,7 +169,7 @@ public abstract partial class SharedXenoArtifactSystem
         if (ent.Comp.Locked)
             return 0;
 
-        return Math.Max(0, ent.Comp.ResearchValue - ent.Comp.ConsumedResearchValue);
+        return ent.Comp.ResearchValue - ent.Comp.ConsumedResearchValue;
     }
 
     /// <summary>
