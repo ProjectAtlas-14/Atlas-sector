e53e77af5711677ed1b730e67ab640e0bab9a792
ivo <mink4@proton.me>
2026-02-10 03:33:52 +0200

    Update ConsentConsition.cs

--- Content.Shared/_Common/Consent/ConsentCondition.cs/ConsentConsition.cs
+++ Content.Shared/_Common/Consent/ConsentCondition.cs/ConsentConsition.cs
@@ -1,26 +1,30 @@
 ﻿// SPDX-FileCopyrightText: Copyright (c) 2025 Space Wizards Federation
 // SPDX-License-Identifier: MIT
 
+using Content.Shared.EntityConditions;
 using Content.Shared.EntityEffects;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Common.Consent.EffectConditions;
 
+// This is dumb. Why, wizden?
+public sealed partial class ConsentEntityConditionSystem : EntityConditionSystem<ConsentComponent, ConsentCondition>
+{
+    [Dependency] private readonly SharedConsentSystem _consent = default!;
+
+    protected override void Condition(Entity<ConsentComponent> entity, ref EntityConditionEvent<ConsentCondition> args) =>
+        args.Result = _consent.HasConsent(entity.Owner, args.Condition.Consent);
+}
+
 /// <summary>
 /// Checks if the target entity has consented to a specific toggle.
 /// </summary>
-public sealed partial class ConsentCondition : EntityEffectCondition
+public sealed partial class ConsentCondition : EntityConditionBase<ConsentCondition>
 {
-    [DataField]
-    public ProtoId<ConsentTogglePrototype> Consent = default!;
+    [DataField(required: true)]
+    public ProtoId<ConsentTogglePrototype> Consent;
 
-    public override bool Condition(EntityEffectBaseArgs args)
-    {
-        return args.EntityManager.System<SharedConsentSystem>()
-            .HasConsent(args.TargetEntity, Consent);
-    }
-
-    public override string GuidebookExplanation(IPrototypeManager prototype)
+    public override string EntityConditionGuidebookText(IPrototypeManager prototype)
     {
         return Loc.GetString("reagent-effect-condition-guidebook-consent-condition", ("consent", Consent));
     }
